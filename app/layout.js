2d6d17cd494bab7bcd16e4acbea11afc7a3bb044
koduwar12 <andyboysama11@gmail.com>
2025-12-14 00:47:49 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'World Cup Pick’em 2026'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: 'sans-serif' }}>
+        {children}
+      </body>
+    </html>
+  )
+}
